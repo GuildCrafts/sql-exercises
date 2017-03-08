14eeab2df11b407bda4f9697da398a914648ea59
Aaron Villanueva <aaronvillanueva@Aarons-MacBook-Pro.local>
2017-03-08 13:11:09 -0800

    loaded

---
+++ sql/04-load-table-student.sql
@@ -0,0 +1,16 @@
+INSERT INTO student.student values (1510, 'Jordan', 9);
+INSERT INTO student.student values (1689, 'Gabriel', 9);
+INSERT INTO student.student values (1381, 'Tiffany', 9);
+INSERT INTO student.student values (1709, 'Cassandra', 9);
+INSERT INTO student.student values (1101, 'Haley', 10);
+INSERT INTO student.student values (1782, 'Andrew', 10);
+INSERT INTO student.student values (1468, 'Kris', 10);
+INSERT INTO student.student values (1641, 'Brittany', 10);
+INSERT INTO student.student values (1274, 'Alexis', 11);
+INSERT INTO student.student values (1316, 'Austin', 11);
+INSERT INTO student.student values (1911, 'Gabriel', 11);
+INSERT INTO student.student values (1501, 'Jessica', 11);
+INSERT INTO student.student values (1304, 'Jordan', 12);
+INSERT INTO student.student values (1025, 'John', 12);
+INSERT INTO student.student values (1934, 'Kyle', 12);
+INSERT INTO student.student values (1661, 'Logan', 12);
---
+++ sql/06-load-table-like.sql
@@ -0,0 +1,10 @@
+	INSERT INTO student.student_like values (1689, 1709);
+	INSERT INTO student.student_like values (1709, 1689);
+	INSERT INTO student.student_like values (1782, 1709);
+	INSERT INTO student.student_like values (1911, 1247);
+	INSERT INTO student.student_like values (1247, 1468);
+	INSERT INTO student.student_like values (1641, 1468);
+	INSERT INTO student.student_like values (1316, 1304);
+	INSERT INTO student.student_like values (1501, 1934);
+	INSERT INTO student.student_like values (1934, 1501);
+	INSERT INTO student.student_like values (1025, 1101);
