7ab91e6ca28d7bb0e906ac3a6c8eecc543cfb2de
Aaron Villanueva <aaronvillanueva@Aarons-MacBook-Pro.local>
2017-03-11 16:50:47 -0800

    exercise 11

--- sql/11-liked-but-does-not-like.sql
+++ sql/11-liked-but-does-not-like.sql
@@ -1,3 +1,25 @@
---11: For every situation where student A likes student B, but we have no information about 
---whom B likes (that is, B's id does not appear in the liker_id column of the like table), 
---return A and B's names and grades. Write the SQL in the file sql/11-liked-but-does-not-like.sql
+SELECT DISTINCT
+	student_a_who_likes_student_b.name,
+	student_a_who_likes_student_b.grade,
+	student_b_who_likes_no_one.name,
+	student_b_who_likes_no_one.grade
+FROM
+	student
+AS
+	student_a_who_likes_student_b
+JOIN
+	student_like
+ON
+	student_a_who_likes_student_b.id = student_like.liker_id
+JOIN
+	student
+AS
+	student_b_who_likes_no_one
+ON
+	student_like.likee_id = student_b_who_likes_no_one.id
+WHERE 
+	student_b_who_likes_no_one.id NOT IN (
+		SELECT liker_id FROM student_like
+		)
+ORDER BY
+	student_a_who_likes_student_b.name;
