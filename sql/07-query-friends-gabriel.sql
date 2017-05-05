7c6c76d0d1ae9c6ca8cfc556500c933e3304f1d3
Stephani McGrath <silentsoundz1977@gmail.com>
2017-05-04 18:02:39 -0700

    implements 07 08 from mini projects and checks off some specs

--- sql/07-query-friends-gabriel.sql
+++ sql/07-query-friends-gabriel.sql
@@ -3,3 +3,4 @@
 SELECT b.name FROM friend
 JOIN student a ON a.id = friend.id1
 JOIN student b ON b.id = friend.id2 WHERE a.name = 'Gabriel';
+
---
+++ sql/09-mutual-likes.sql
@@ -0,0 +1,1 @@
+-- 09: For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order. 
