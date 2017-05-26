b033a401ecee7c9e1ea33d2bf10ebbe1ba886e25
Brandon Gilchrist <brandongilchrist@LGs-MacBook-Pro-Brandon.local>
2017-05-26 12:40:45 -0700

    redid AppImpersonator and finished famous people

--- khan-academy/modifying_databases.sql
+++ khan-academy/modifying_databases.sql
@@ -1,13 +1,16 @@
 -- Solution SQL for the project from the Modifying databases with SQL course ("App impersonator")
-COPY world 
-FROM '../data/world.sql'
--- Query to find population per square mile
-SELECT DISTINCT country.name AS "Country Name", 
-  country.continent AS "Continent", 
-  country.population AS "Total Population", 
-  round(country.surfacearea::DECIMAL, 2) AS "Square Miles",
-  round (country.population / country.surfacearea::DECIMAL, 2) AS "Population Per Mile" 
-  FROM countrylanguage
-JOIN country ON countrylanguage.countrycode = country.code
-JOIN city ON city.countrycode = country.code;
---- query result at '../data/queryresults.csv'
+CREATE TABLE `city` (
+  ID INTEGER,
+  Name TEXT,
+  CountryCode TEXT,
+  District TEXT);
+
+INSERT INTO `city` VALUES
+(1,'Kabul','AFG','Kabol'),
+(2,'Qandahar','AFG','Qandahar'),
+(3,'Herat','AFG','Herat'),
+(4,'Mazar-e-Sharif','AFG','Balkh'),
+(5,'Amsterdam','NLD','Noord-Holland');
+
+UPDATE city SET District = 'North-Holland' WHERE District = 'Noord-Holland';
+DELETE FROM city WHERE id = 1;
