7e0d262f061ccdfc0c9e7d9598517eb0348cde6f
Brandon Gilchrist <brandongilchrist@LGs-MacBook-Pro-Brandon.local>
2017-05-26 11:42:35 -0700

    finished App Impersonator and uploaded Dataset and Results

--- khan-academy/modifying_databases.sql
+++ khan-academy/modifying_databases.sql
@@ -1,1 +1,13 @@
 -- Solution SQL for the project from the Modifying databases with SQL course ("App impersonator")
+COPY world 
+FROM '../data/world.sql'
+-- Query to find population per square mile
+SELECT DISTINCT country.name AS "Country Name", 
+  country.continent AS "Continent", 
+  country.population AS "Total Population", 
+  round(country.surfacearea::DECIMAL, 2) AS "Square Miles",
+  round (country.population / country.surfacearea::DECIMAL, 2) AS "Population Per Mile" 
+  FROM countrylanguage
+JOIN country ON countrylanguage.countrycode = country.code
+JOIN city ON city.countrycode = country.code;
+--- query result at '../data/queryresults.csv'
